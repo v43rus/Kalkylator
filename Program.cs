c338beaca4d804fe9ef741cd3f047ed87bd4dccd
v43rus <leonard.jm@live.com>
2023-12-11 11:27:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Kalkylator
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Kalkylator());
+
+            // Välkomnande meddelande
+            // En lista för att spara historik för räkningar
+            // Användaren matar in tal och matematiska operation
+            // OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+            // Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
+            // Lägga resultat till listan
+            // Visa resultat
+            // Fråga användaren om den vill visa tidigare resultat.
+            // Visa tidigare resultat
+            // Fråga användaren om den vill avsluta eller fortsätta.
+
+        }
+    }
+}
